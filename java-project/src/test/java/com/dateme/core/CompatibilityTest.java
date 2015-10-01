310d2d7c5a7883835e1bdb77c54efbdd422419f8
Timothy Kim <timothy.kim@washpost.com>
2015-10-01 17:06:01 -0400

    add compatibility test method

---
+++ java-project/src/test/java/com/dateme/core/CompatibilityTest.java
@@ -0,0 +1,32 @@
+package com.dateme;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CompatibilityTest {
+
+    @Test
+    public void perfectCompatibilityTest() {
+        User user1 = new User("a@b.com", new RGB(255, 0, 0), 50);
+        User user2 = new User("b@c.com", new RGB(255, 0, 0), 50);
+        int score = LoveEngine.compatibility(user1, user2);
+        Assert.assertEquals(10, score);
+    }
+
+    @Test
+    public void numberDiffCompatibilityTest() {
+        User user1 = new User("a@b.com", new RGB(255, 0, 0), 1);
+        User user2 = new User("b@c.com", new RGB(255, 0, 0), 10);
+        int score = LoveEngine.compatibility(user1, user2);
+
+        Assert.assertEquals(1, score);
+
+        User user3 = new User("a@b.com", new RGB(255, 0, 0), 5);
+        User user4 = new User("b@c.com", new RGB(255, 0, 0), 100);
+        int score2 = LoveEngine.compatibility(user3, user4);
+
+        Assert.assertEquals(5, score2);
+
+    }
+
+}
